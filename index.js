93e27a346333d2bc2b624b8893bc8be24d7f22af
slystersanto <santhosh.mech.19@gmail.com>
2023-04-26 11:42:41 +0530

    2nd commit

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const app = express();
 const cors = require('cors');
 const bcryptjs = require('bcryptjs');
 app.use(express.json());
-app.use(
-    cors({
-      origin: "http://localhost:3000",
-    })
-  );
+
+app.use((req, res, next) => {
+   
+  res.setHeader('Access-Control-Allow-Origin', 'https://frolicking-sorbet-e4b2eb.netlify.app','http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// app.use(
+//     cors({
+//       origin: "https://frolicking-sorbet-e4b2eb.netlify.app/",
+//     })
+//   );
 const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
 const dotenv = require('dotenv').config();
